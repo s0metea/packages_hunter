11ef190d286005fe5db0b1101bb9663cc93550a7
mike <m.burov@pm.me>
2022-05-11 16:28:38 +0300

    Realtime scanning added Colors added Code refactoring

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import requests
 import semver
 import networkx as nx
 
+
+class BColors:
+    HEADER = '\033[40m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 # Will be populated in runtime
 DOMAINS_GLOBAL = {
     "gmail.com": []
@@ -45,7 +58,7 @@ def get_maintainers(package_info):
         for field in fields_of_interest:
             if field in package_info:
                 maintainers.append(package_info[field])
-    return package_info['name']
+    return maintainers
 
 
 # Actors contain various actors like 'author', '_npmUser', 'maintainers', so we iterate over them
@@ -74,21 +87,16 @@ def extract_domains(emails):
                 DOMAINS_GLOBAL[domain].append(email)
         else:
             DOMAINS_GLOBAL[domain] = [email]
-            domains.append(domain)
+            if domain not in get_restricted_domains():
+                domains.append(domain)
     return domains
 
 
-def filter_emails(emails):
+# Skip scanning for domains returned by this function
+def get_restricted_domains():
     # We are looking for custom domains so filtering well known services
-    domains_to_filter = {'gmail.com'}
-    filtered = []
-    for email in emails:
-        domain = email[email.find('@') + 1:]
-        if domain not in domains_to_filter and domain not in filtered:
-            filtered.append(domain)
-    if len(filtered) > 0:
-        print(filtered)
-    return filtered
+    domains_to_filter = ['gmail.com']
+    return domains_to_filter
 
 
 def get_dependencies(package_data):
@@ -107,12 +115,16 @@ def check_domain_for_availability(domain):
     base_domain = get_fld(domain, fix_protocol=True)
     url = f"https://secure.domain.com/register/pages/dom_lookup_json.cmp?autoAdd=false&search_type=standard&dom_lookup={base_domain}"
     domain_status = requests.get(url).json()
-    is_available = 0
+    is_available_for_purchase = 0
     for offer in domain_status:
         if offer['domain'] == base_domain:
-            is_available = offer['availability']
+            is_available_for_purchase = offer['availability']
+            if is_available_for_purchase:
+                print(f'    {BColors.OKBLUE} {domain} {BColors.OKGREEN}is available {BColors.ENDC}')
+            else:
+                print(f'    {BColors.HEADER} {domain} already taken {BColors.ENDC}')
             break
-    return is_available
+    return is_available_for_purchase
 
 
 def get_available_domains(domains):
@@ -126,7 +138,6 @@ def get_available_domains(domains):
 def get_domains_from_package_data(package_data):
     maintainers = get_maintainers(package_data)
     emails = extract_emails(maintainers)
-    # filtered_emails = filter_emails(emails)
     domains_for_checking = extract_domains(emails)
     return domains_for_checking
 
@@ -135,47 +146,48 @@ def get_domains_from_package(package, version):
     package_data = get_npm_package_info(package, version)
     maintainers = get_maintainers(package_data)
     emails = extract_emails(maintainers)
-    filtered_emails = filter_emails(emails)
-    domains_for_checking = extract_domains(filtered_emails)
+    domains_for_checking = extract_domains(emails)
     return domains_for_checking
 
 
-# Nice to have, TBD
+# TODO: Nice to have it
 def build_graph(package, version):
     package_data = get_npm_package_info(package, version)
     dependencies = get_dependencies(package_data)
     for dependency in dependencies:
         package_data = get_npm_package_info(dependency[0], dependency[1])
-        g.add_edge(package_data, dependency, weight=1)
+        # g.add_edge(package_data, dependency, weight=1)
         build_graph(dependency[0], dependency[1])
 
 
 def get_domains_from_package_deep(package, version, depth):
-    package_data = get_npm_package_info(package, version)
-    this_domains = get_domains_from_package_data(package_data)
-    # If depth specified, we will end recursion
     if depth <= 0:
-        return this_domains
+        return []
+
+    package_data = get_npm_package_info(package, version)
+    print(f'{BColors.BOLD}{package} {version}:{BColors.ENDC}')
 
     dependencies = []
+    this_domains = []
 
     if 'versions' in package_data:
         for ver in package_data['versions']:
-            g.add_edge((package_data['name'], version), (package_data['versions'][ver]['name'], ver), weight=depth)
+            #g.add_edge((package_data['name'], version), (package_data['versions'][ver]['name'], ver), weight=depth)
             dependencies = dependencies + get_dependencies(package_data['versions'][ver])
     else:
         if '_id' in package_data:
             if package_data['_id'] in DEPS.keys():
-                return this_domains
+                return []
+            this_domains = get_domains_from_package_data(package_data)
+            get_available_domains(this_domains)
             dependencies = get_dependencies(package_data)
             DEPS[package_data['_id']] = dependencies
 
     for dependency in set(dependencies):
-        g.add_edge((package, version), dependency, weight=depth)
+        # g.add_edge((package, version), dependency, weight=depth)
         # print(f"Depth: {depth}/{package}:{version}: {dependency[0]}:{dependency[1]}")
         this_domains = this_domains + get_domains_from_package_deep(dependency[0], dependency[1].strip('>=^~'),
                                                                     depth - 1)
-
     return this_domains
 
 
@@ -204,7 +216,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Explore who maintains the dependencies you are using')
     parser.add_argument('-p', '--pkg', dest='package', help='Package in name:version format')
     parser.add_argument('-f', '--file', dest='file', help='File with packages in name:version format')
-    parser.add_argument('-d', '--depth', dest='depth', help='Scanning depth', default=10)
+    parser.add_argument('-d', '--depth', dest='depth', help='Scanning depth', default=100)
+    # parser.add_argument('-s', '--silent', dest='silent', help='Passive scanning without domains resolving')
     # parser.add_argument('-E', '--ext', dest='file', help='Extended scanning with packages in name:version format')
 
     args = parser.parse_args()
@@ -212,29 +225,34 @@ if __name__ == "__main__":
         package = args.package.split(sep=':')
         result = scan_single_package(package[0], package[1])
         print(result)
+        exit(0)
 
+    elif args.file:
+        with open('packages.txt', 'r', encoding='utf-8') as f:
+            packages = f.readlines()
     else:
         packages = [('@cloudblueconnect/connect-jsdoc-theme', '19.2.1')]
-        is_available = {}
-        for package in packages:
-            domains = get_domains_from_package_deep(package[0], package[1], depth=args.depth)
-            is_available[package] = get_available_domains(domains)
-        print("Finally, we are finished. Now lets check what we found:")
-        for package in is_available:
-            for domain in is_available[package]:
-                if domain in DOMAINS_GLOBAL:
-                    print(
-                        f'{domain} is available for purchase. Mailserver on this domain contains following emails: {DOMAINS_GLOBAL[domain]}')
-        print(DOMAINS_GLOBAL)
-        print(set(domains))
-
-        # domains = load_raw_domains('domains.txt')
-        # fld_list = []
-        # for domain in domains:
-        #     fld = get_fld(domain.strip(), fix_protocol=True)
-        #     fld_list.append(fld)
-        # fld_set = set(fld_list)
-        # with open('result.txt', 'w', encoding='utf-8') as f:
-        #     for domain in fld_set:
-        #         f.write(domain + '\n')
-
+                    # ("core-js", "3.6.5"), ("password-generator", "2.3.2"),
+                    # ("vue", "2.6.11"), ("vue-class-component", "7.2.3"),
+                    # ("vue-property-decorator", "9.1.2"), ("vuex", "3.4.0"), ("wait-for-expect", "3.0.2")]
+    is_available = {}
+    for package in packages:
+        domains = get_domains_from_package_deep(package[0], package[1], depth=args.depth)
+        is_available[package] = get_available_domains(domains)
+    print("Finally, we are finished. Lets check what we found:")
+    for package in is_available:
+        for domain in is_available[package]:
+            print(f'{BColors.BOLD}{domain}{BColors.OKGREEN} is available for purchase{BColors.ENDC}')
+            if domain in DOMAINS_GLOBAL:
+                print(f'Mailserver on this domain contains following emails: {DOMAINS_GLOBAL[domain]}')
+    exit(0)
+    # print(DOMAINS_GLOBAL)
+    # domains = load_raw_domains('domains.txt')
+    # fld_list = []
+    # for domain in domains:
+    #     fld = get_fld(domain.strip(), fix_protocol=True)
+    #     fld_list.append(fld)
+    # fld_set = set(fld_list)
+    # with open('result.txt', 'w', encoding='utf-8') as f:
+    #     for domain in fld_set:
+    #         f.write(domain + '\n')
